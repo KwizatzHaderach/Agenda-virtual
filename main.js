e5c2a719e60b2003b0446eb22d05be961d41dce1
Mauricio de Mello <mau-ideias@hotmail.com>
2023-05-29 23:48:43 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('form-contato');
+const tabelaContatos = document.querySelector('tfoot');
+const nomes = [];
+const numeros = [];
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+
+    function adicionaLinha() {
+        const inputNome = document.getElementById('nome').value;
+        const inputNumero = parseInt(document.getElementById('numero').value);
+
+        nomes.push(inputNome);
+        numeros.push(inputNumero);
+
+        let linha = '<tr>';
+        linha += `<td>${inputNome}</td>`;
+        linha += `<td>${inputNumero}</td>`;
+        linha += '</tr>';
+
+        linhas += linha;
+    }
